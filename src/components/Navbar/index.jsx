29eb17bdde03428ecd6a892a32d932c25c469719
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-10-31 23:46:24 +0530

    feature added: hamburger menu for smaller viewports

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,17 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.css';
-import Logo from '../../assets/images/main-logo.png';
+import Logo from '../../assets/images/lightlogo.svg';
+import {useState} from 'react';
+import './mediaqueries.css';
 
 const Navbar = () => {
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!isMenuOpen);
+    };
+
     return (
         <div className='container'>
             <div className='nav-container'>
@@ -11,7 +20,7 @@ const Navbar = () => {
                 </Link>
             </div>
             <div className='nav-bar'>
-                <nav>
+                <nav className='desktop-nav'>
                     <ul className="nav-list">
                         <li className="nav-item">
                             <NavLink exact="true" activeclassname="active" className="nav-link" to='/'>
@@ -34,6 +43,38 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                     </ul>
+                </nav>
+                <nav className={`hamburger-nav ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                    <div className='hamburger-menu'>
+                        <div className='hamburger-icon' onClick={toggleMenu}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div className= {`menu-links ${isMenuOpen ? 'open' : ''} `}>
+                            <li className="nav-item">
+                                <NavLink exact="true" onClick={toggleMenu} activeclassname="active" className="navham-link" to='/'>
+                                    About
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink exact="true" onClick={toggleMenu} activeclassname="active" className="navham-link" to='/'>
+                                    Education
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink exact="true" onClick={toggleMenu} activeclassname="active" className="navham-link" to='/'>
+                                    Projects
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink exact="true" onClick={toggleMenu} activeclassname="active" className="navham-link" to='/'>
+                                    Contact
+                                </NavLink>
+                            </li>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </div>
