1a176aed4de014ab496121daa79182a08fc64f57
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-11-01 12:48:41 +0530

    Minor feature changes: Toggle off for clicks outside hamburger menu. CSS change.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,31 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.css';
 import Logo from '../../assets/images/lightlogo.svg';
-import {useState} from 'react';
+import {useState, useRef, useEffect} from 'react';
 import './mediaqueries.css';
 
 const Navbar = () => {
 
     const [isMenuOpen, setMenuOpen] = useState(false);
+    const containerRef = useRef(null);
 
     const toggleMenu = () => {
       setMenuOpen(!isMenuOpen);
     };
+
+    const handleOutsideClick = (e) => {
+        if (containerRef.current && !containerRef.current.contains(e.target)) {
+          setMenuOpen(false);
+        }
+      };
+
+      useEffect(() => {
+        document.addEventListener('mousedown', handleOutsideClick);
+        return () => {
+          document.removeEventListener('mousedown', handleOutsideClick);
+        };
+      }, []);
+    
 
     return (
         <div className='container'>
@@ -44,7 +59,7 @@ const Navbar = () => {
                         </li>
                     </ul>
                 </nav>
-                <nav className={`hamburger-nav ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                <nav className={`hamburger-nav ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu} ref={containerRef}>
                     <div className='hamburger-menu'>
                         <div className='hamburger-icon' onClick={toggleMenu}>
                             <span></span>
