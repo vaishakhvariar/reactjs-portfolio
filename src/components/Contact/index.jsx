c805875ce3e03142192c0c0741ccc5eaac6ff61e
Vaishakh Variar <vaishakhvariar@gmail.com>
2023-11-03 17:43:44 +0530

    feat: Contact Form updated

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,35 +1,89 @@
 import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBlogger, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+
+    const form = useRef()
+    const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm('service_lvo4nbb', 'template_ilirxzh', form.current, 'Z8-8YvW5AsptXYCjs')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
+
     return (
     <div className='contact-container'>
         <div className='form-container'>
             <p> Get in Touch </p>
         <h1> Contact Me </h1> 
-        <p> <a href='mailto:vaishakhvariar@gmail.com'> vaishakhvariar@gmail.com </a></p>  
+        <p> <a className='email-link' href='mailto:vaishakhvariar@gmail.com'> vaishakhvariar@gmail.com </a></p>  
+        <form className='form-main' ref={form} onSubmit={sendEmail}>
+              <ul className='form-elements-list'>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li className='full'>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li className='full'>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="submit-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
         </div>
         <div className='socials-container'>
         <ul className='social-links'>
-            <li>
-                <a className='so-link' target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/vaishakhvariar/'> 
+            <li className='so-link'>
+                <a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/vaishakhvariar/'> 
                     <FontAwesomeIcon icon={(faLinkedin)} color='#4d4d4e' />
                 </a>
             </li>
-            <li>
-                <a className='so-link' target='_blank' rel='noreferrer' href='https://github.com/vaishakhvariar'> 
+            <li className='so-link'>
+                <a target='_blank' rel='noreferrer' href='https://github.com/vaishakhvariar'> 
                     <FontAwesomeIcon icon={(faGithub)} color='#4d4d4e' />
                 </a>
             </li>
            
-            <li>
-                <a className='so-link' target='_blank' rel='noreferrer' href='https://vvariarwrites.wordpress.com/'> 
+            <li className='so-link'>
+                <a target='_blank' rel='noreferrer' href='https://vvariarwrites.wordpress.com/'> 
                     <FontAwesomeIcon icon={(faBlogger)} color='#4d4d4e' />
                 </a>
             </li>
-            <li>
-                <a className='so-link' target='_blank' rel='noreferrer' href=' https://www.instagram.com/vaishakh_variar/'> 
+            <li className='so-link'>
+                <a  target='_blank' rel='noreferrer' href=' https://www.instagram.com/vaishakh_variar/'> 
                     <FontAwesomeIcon icon={(faInstagram)} color='#4d4d4e' />
                 </a>
             </li>
